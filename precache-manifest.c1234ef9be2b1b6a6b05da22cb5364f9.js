8e29f6b343819a6dbc6b11133a370369f990bce3
Sanshain <my-email@ya.ru>
2020-08-13 21:57:10 +0300

    All files

---
+++ precache-manifest.c1234ef9be2b1b6a6b05da22cb5364f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a30c48c0d155cbffef05fdf0a7122b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aa09284d200711450f3",
+    "url": "/static/js/2.b20b3457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b20b3457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283ef6c17b4a40d9f737",
+    "url": "/static/js/main.54508c69.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
